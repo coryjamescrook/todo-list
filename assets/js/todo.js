b646dbfb7c8f2bb3f000b824c254b3ac8aa9a94f
Cory Revesz <coryrevesz@gmail.com>
2017-11-20 15:31:58 -0500

    First Commit

---
+++ assets/js/todo.js
@@ -0,0 +1,47 @@
+// Check off todos by clicking //
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("done");
+});
+
+//click on x to delete
+$("ul").on("click",".del", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	setFocus();
+	event.stopPropagation();
+});
+
+$("ul").on("click",".fav", function(event) {
+	$(this).children('.fa-star-o, .fa-star').toggleClass('fa-star fa-star-o');
+	setFocus();
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li title='" + todoText + "'><span class='del'><i class='fa fa-trash'></i></span> " + todoText + "<span class='fav'><i class='fa fa-star-o'></span></li>");
+	}
+});
+
+$(".fa-plus").on("click", function() {
+	$("input[type='text']").fadeToggle("fast");
+	setFocus();
+});
+
+$(document).ready(function() {
+	setFocus();
+});
+
+function setFocus() {
+	$("input[type='text']").focus();
+};
+
+$("input[type='text']").on("blur", setFocus);
+
+$( function() {
+	$( ".sortable" ).sortable();
+	$( ".sortable" ).disableSelection();
+} );
